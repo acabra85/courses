b3cb6774fcb441e78a3de1d1736d2f92dc3c249e
Agustin Cabra <cabra.agustin@gmail.com>
2023-01-30 23:45:14 +0100

    Solved percolation

--- algorithmsQ1-2023/assignment_01/Percolation.java
+++ algorithmsQ1-2023/assignment_01/Percolation.java
@@ -1,37 +1,195 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
+
+    private final int n;
+    private int openSites;
+    private final Site[][] grid;
+    private final WeightedQuickUnionUF qf;
+    private final Site[] tops;
+    private final Site[] botts;
+
+    private static class Site {
+        private boolean empty;
+        boolean open;
+        int id;
+
+        Site(int id) {
+            this.open = false;
+            this.id = id;
+            this.empty = true;
+        }
+
+        public void doOpen() {
+            this.open = true;
+        }
+
+        public boolean isOpen() {
+            return this.open;
+        }
+
+        public boolean isFull() {
+            return this.open && !this.isEmpty();
+        }
+
+        public boolean isEmpty() {
+            return this.empty;
+        }
+
+        public boolean isBlocked() {
+            return !this.open;
+        }
+
+        public void doFill() {
+            this.empty = false;
+        }
+    }
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
+        Percolation.validateN(n);
+        this.grid = Percolation.buildGrid(n);
+        this.n = n;
+        this.qf = new WeightedQuickUnionUF(n*n);
+        this.tops = getRow(0);
+        this.botts = getRow(n-1);
+    }
 
+    private Site[] getRow(int row) {
+        return this.grid[row];
+    }
+
+    private static Site[][] buildGrid(int n) {
+        Site[][] sites = new Site[n][n];
+        int id = -1;
+        for (int i = 0; i < n; ++i) {
+            Site[] row = new Site[n];
+            for (int j = 0; j < n; ++j) {
+                row[j] = new Site(++id);
+            }
+            sites[i] = row;
+        }
+        return sites;
+    }
+
+    private static void validateN(int n) {
+        if(n <= 0 || n > 1000) {
+            throw new IllegalArgumentException("n must be between [1,1000], received: " + n);
+        }
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
+        int r = row - 1;
+        int c = col - 1;
+        Percolation.validateArguments(this.n, r, c);
+        Site site = grid[r][c];
+        if(site.isBlocked()) {
+            processOpening(site, r, c);
+            ++this.openSites;
+        }
+    }
 
+    final static int[][] NEIGHBORS = {
+            {0, -1},
+            {0, 1},
+            {-1, 0},
+            {1, 0},
+    };
+
+    private void processOpening(Site site, int r, int c) {
+        site.doOpen();
+        // any open site on the top will be filled
+        if (r == 0) {
+            site.doFill();
+            for (int[] nCoord : NEIGHBORS) {
+                Site neighbor = getSite(r + nCoord[0], c + nCoord[1]);
+                if (neighbor != null && neighbor.isOpen()) {
+                    qf.union(site.id, neighbor.id);
+                    neighbor.doFill();
+                }
+            }
+        } else {
+            for (int[] nCoord : NEIGHBORS) {
+                Site neighbor = getSite(r + nCoord[0], c + nCoord[1]);
+                if (neighbor != null && neighbor.isOpen()) {
+                    if(site.isFull() || neighbor.isFull()) {
+                        site.doFill();
+                        neighbor.doFill();
+                    }
+                    qf.union(site.id, neighbor.id);
+                }
+            }
+        }
+    }
+
+    private Site getSite(int r, int c) {
+        if(validInput(n, r) && validInput(n, c)) {
+            return this.grid[r][c];
+        }
+        return null;
+    }
+
+    private static boolean validInput(int n, int row) {
+        return row >= 0 && row < n;
+    }
+
+    private static void validateArguments(int n, int row, int col) {
+        if(!validInput(n, row)) {
+            throw new IllegalArgumentException(String.format("row must be between [1,%d], received: %d", n, row));
+        }
+        if(!validInput(n, col)) {
+            throw new IllegalArgumentException(String.format("col must be between [1,%d], received: %d", n, col));
+        }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return false;
+        validateArguments(n, row, col);
+        return this.grid[row-1][col-1].isOpen();
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        return false;
+        validateArguments(n, row, col);
+        return this.grid[row-1][col-1].isFull();
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        return -1;
+        return this.openSites;
     }
 
     // does the system percolate?
     public boolean percolates() {
+        for (Site top: this.tops) {
+            for (Site bott : this.botts) {
+                if(qf.find(top.id) == qf.find(bott.id)) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     // test client (optional)
     public static void main(String[] args) {
         System.out.println("hello percolator");
+        Percolation percolation = new Percolation(8);
+        System.out.printf("Open sites: %d%n", percolation.numberOfOpenSites());
+
+        System.out.println("Percolates :" + percolation.percolates());
+
+        percolation.open(1,1);
+        percolation.open(2,1);
+        percolation.open(3,1);
+        percolation.open(4,1);
+        percolation.open(5,1);
+        percolation.open(6,1);
+        percolation.open(7,1);
+        percolation.open(7,2);
+        percolation.open(8,2);
+        System.out.printf("Open sites: %d%n", percolation.numberOfOpenSites());
+        System.out.println("Percolates :" + percolation.percolates());
     }
 }
