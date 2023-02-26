fa6f242dd7b173aa61d5485ad816f1930a38d781
Agustin Cabra <cabra.agustin@gmail.com>
2023-02-26 13:53:49 +0100

    start assigment FastCollinear

---
+++ algorithmsQ1-2023/assignment_03/FastCollinearPoints.java
@@ -0,0 +1,13 @@
+public class FastCollinearPoints {
+    public FastCollinearPoints(Point[] points) {
+
+    }
+
+    public LineSegment[] segments() {
+        return new LineSegment[0];
+    }
+
+    public int numberOfSegments() {
+        return 0;
+    }
+}
